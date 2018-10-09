a136ae1a362dc96dfa5302eb92d1c325525d97f1
Harsh <akshatrana2007@gmail.com>
2018-10-08 21:38:32 -0500

    made use of sort /select function

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ async function createCourse() {
 }
 
 async function getCourses(){
+    const courses = await Course
+    .find({ author: 'Harsh', isPublished: true })
+    .limit(10)
+    .sort({ name: 1})
+    .select({name: 1, tags: 1});
+    console.log(courses);
 
 }
 getCourses();

b6da20e61bdd04045c830fe7c6ae3078dcfebef5
Harsh <akshatrana2007@gmail.com>
2018-10-08 23:53:07 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function createCourse() {
 async function getCourses(){
     const courses = await Course
     .find({ author: 'Harsh', isPublished: true })
+   // .find({ price: { $gte: 10, } })
     .limit(10)
     .sort({ name: 1})
     .select({name: 1, tags: 1});
